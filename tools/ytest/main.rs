aa5747b48763263f9a858ebddbced1dd8036174b
TheCPP <buch.toni@outlook.com>
2024-09-03 20:26:57 +0200

    [TOOLS] implementing ygen testing tool (ytest)

--- tools/ytest/main.rs
+++ tools/ytest/main.rs
@@ -1,3 +1,101 @@
+use std::{fs::File, io::Read, process::{exit, Command}};
+
+use ygen::Support::{Cli, Colorize};
+
+mod parse;
+use parse::*;
+
 fn main() {
-    println!("Hello World!");
+    let mut cli = Cli::new(
+        "ytest", "The testing tool for ygen", "1.0", "Cr0a3"
+    );
+    
+    cli.add_opt("h", "help", "Displays help");
+    cli.add_opt("v", "version", "Displays the version");
+
+    cli.add_arg("t", "test", "The file to the testcase", true);
+
+    cli.scan();
+
+    if cli.opt("h") {
+        cli.help();
+    } else if cli.opt("v") {
+        cli.version();
+    }
+
+    let file = cli.arg_val("t").expect("We said it was required");
+    let mut file = match File::open(file) {
+        Ok(f) => f,
+        Err(err) => {
+            println!("{}: {}", "Error".red().bold(), err);
+            exit(-1)
+        },
+    };
+
+    let mut buf = String::new();
+    match file.read_to_string(&mut buf) {
+        Ok(_) => {},
+        Err(err) => {
+            println!("{}: {}", "Error".red().bold(), err);
+            exit(-1)
+        },
+    };
+
+    let parsed = parse(buf);
+    
+    let args = parsed.cmd.replace("%s", &format!("\"{}\"", &parsed.input));
+    let mut args = args.split(" ").collect::<Vec<&str>>();
+
+    let program = args.get(0).expect("expected valid excutable name").to_string();
+
+    args.reverse();
+    args.pop();
+    args.reverse();
+
+
+    println!("{}: executing following commandline: '{}{}'", "Info".blue().bold(), program, {
+        let mut fmt = String::new();
+
+        for arg in &args {
+            fmt.push_str(&format!(" {}", arg));
+        }
+
+        fmt
+    });
+
+    let mut cmd = Command::new( program );
+    
+    for arg in args {
+        cmd.arg(arg);
+    }
+
+    match cmd.spawn() {
+        Ok(cmd) => cmd,
+        Err(err) => {
+            println!("{}: {}", "Error".red().bold(), err);
+            exit(-1)
+        }
+    };
+
+    let out = cmd.output().expect("failed to execute process");
+
+    if out.stdout != parsed.expected_out.as_bytes().to_vec() {
+        println!("{}: expected stdout didn't match real stdout", "Error".red().bold());
+        println!("\texpected: \"{:#?}\"\n", out.stdout.iter()                                      
+            .filter_map(|&byte| {
+                if byte >= 32 && byte <= 126 {
+                    Some(byte as char)
+                } else {
+                    None
+                }
+            }).collect::<String>());
+        println!("\tfound: \"{:#?}\"\n", parsed.expected_out.as_bytes().to_vec().iter()                                      
+            .filter_map(|&byte| {
+                if byte >= 32 && byte <= 126 {
+                    Some(byte as char)
+                } else {
+                    None
+                }
+            }).collect::<String>());
+    }
 }
---
+++ tools/ytest/parse.rs
@@ -0,0 +1,38 @@
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Parsed {
+    pub cmd: String,
+    pub input: String,
+    pub expected_out: String
+}
+
+pub fn parse(input: String) -> Parsed { 
+    let mut out = Parsed {
+        cmd: String::new(),
+        input: String::new(),
+        expected_out: String::new(),
+    };
+    
+    let mut append ;
+
+    for line in input.lines() {
+        append = true;
+
+        if line.trim_start().starts_with("# RUN:") {
+            out.cmd += &line.replace("# RUN:", "");
+            append = false;
+        }
+        
+        if line.trim_start().starts_with("# STDOUT:") {
+            out.cmd += &line.replace("# STDOUT:", "");
+            append = false;
+        }
+
+        if append {
+            out.input.push_str(line);
+        }
+    }
+
+    out.cmd = out.cmd.trim_start().to_string();
+
+    out
+}
