7268b8a1cb6773d31ad341ea6fc3dabc875284c5
TheCPP <buch.toni@outlook.com>
2024-09-04 21:21:11 +0200

    [FIX] fixing component directory

---
+++ web/components/infoBox.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export const InfoBox = ({ name, descr, link }: { name: string, descr: string, link: string }) => {
+    return (
+      <Link
+        href={link}
+        className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        rel="noopener noreferrer"
+      >
+        <h2 className="mb-3 text-2xl font-semibold">
+          {name}
+          <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            -&gt;
+          </span>
+        </h2>
+        <p className="m-0 max-w-[30ch] text-sm opacity-50">
+          {descr}
+        </p>
+      </Link>
+    );
+  }
