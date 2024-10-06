87d677f4149236286486eecc0618cbb85a3ae54b
TheCPP <buch.toni@outlook.com>
2024-10-06 14:56:58 +0200

    [TYPES] adding u8/i8

--- src/IR/typ.rs
+++ src/IR/typ.rs
@@ -5,6 +5,8 @@ use std::fmt::Display;
 /// If you want an empty Type consider using `TypeMetadata`
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Type {
+    /// Just an u8 with a value
+    u8(u8),
     /// Just an u16 with a value
     u16(u16),
     /// Just an u32 with a value
@@ -12,6 +14,8 @@ pub enum Type {
     /// Just an u64 with a value
     u64(u64),
 
+    /// Just an i8 with a value
+    i8(i8),
     /// Just an i16 with a value
     i16(i16),
     /// Just an i32 with a value
@@ -28,6 +32,8 @@ pub enum Type {
 /// Stores type metadata (just the type without data)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum TypeMetadata {
+    /// u8
+    u8,
     /// u16
     u16,
     /// u32
@@ -35,6 +41,8 @@ pub enum TypeMetadata {
     /// u64
     u64,
 
+    /// i8
+    i8,
     /// i16
     i16,
     /// i32
@@ -60,6 +68,8 @@ impl Type {
             Type::i64(val) => *val as u64,
             Type::ptr(adr) => *adr as u64,
             Type::Void => 0,
+            Type::u8(val) => *val as u64,
+            Type::i8(val) => *val as u64,
         }
     }
 
@@ -74,6 +84,8 @@ impl Type {
             TypeMetadata::i64 => Type::i64(value as i64),
             TypeMetadata::ptr => Type::ptr(value as i64),
             TypeMetadata::Void => Type::Void,
+            TypeMetadata::u8 => Type::u8(value as u8),
+            TypeMetadata::i8 => Type::i8(value as i8),
         }
     }
 }
@@ -82,6 +94,7 @@ impl TypeMetadata {
     /// Returns the size of the type in bits
     pub fn bitSize(&self) -> usize {
         match self {
+            TypeMetadata::u8 | TypeMetadata::i8 => 8,
             TypeMetadata::u16 | TypeMetadata::i16 => 16,
             TypeMetadata::u32 | TypeMetadata::i32 => 32,
             TypeMetadata::u64 | TypeMetadata::i64 => 64,
@@ -113,10 +126,12 @@ impl TypeMetadata {
     /// returns the parsed typemetadata
     pub fn parse(string: String) -> Option<Self> {
         match string.as_str() {
+            "u8" => Some(TypeMetadata::u8),
             "u16" => Some(TypeMetadata::u16),
             "u32" => Some(TypeMetadata::u32),
             "u64" => Some(TypeMetadata::u64),
 
+            "i8" => Some(TypeMetadata::i8),
             "i16" => Some(TypeMetadata::i16),
             "i32" => Some(TypeMetadata::i32),
             "i64" => Some(TypeMetadata::i64),
@@ -133,12 +148,16 @@ impl TypeMetadata {
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", match &self {
+            Type::u8(i) => format!("u8 {}", i),
             Type::u16(i) => format!("u16 {}", i),
             Type::u32(i) => format!("u32 {}", i),
             Type::u64(i) => format!("u64 {}", i),
+
+            Type::i8(i) => format!("i8 {}", i),
             Type::i16(i) => format!("i16 {}", i),
             Type::i32(i) => format!("i32 {}", i),
             Type::i64(i) => format!("i64 {}", i),
+
             Type::ptr(adr) => format!("ptr {:#04x}", adr),
             Type::Void => format!("void"),
         })
@@ -148,12 +167,16 @@ impl Display for Type {
 impl Display for TypeMetadata {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", match &self {
+            TypeMetadata::u8 => "u8",
             TypeMetadata::u16 => "u16",
             TypeMetadata::u32 => "u32",
             TypeMetadata::u64 => "u64",
+
+            TypeMetadata::i8 => "i8",
             TypeMetadata::i16 => "i16",
             TypeMetadata::i32 => "i32",
             TypeMetadata::i64 => "i64",
+
             TypeMetadata::ptr => "ptr",
             TypeMetadata::Void => "void",
         })
@@ -163,12 +186,16 @@ impl Display for TypeMetadata {
 impl From<Type> for TypeMetadata {
     fn from(value: Type) -> Self {
         match value {
+            Type::u8(_) => TypeMetadata::u8,
             Type::u16(_) => TypeMetadata::u16,
             Type::u32(_) => TypeMetadata::u32,
             Type::u64(_) => TypeMetadata::u64,
+
+            Type::i8(_) => TypeMetadata::i8,
             Type::i16(_) => TypeMetadata::i16,
             Type::i32(_) => TypeMetadata::i32,
             Type::i64(_) => TypeMetadata::i64,
+
             Type::ptr(_) => TypeMetadata::ptr,
             Type::Void => TypeMetadata::Void,
         }
@@ -178,12 +205,16 @@ impl From<Type> for TypeMetadata {
 impl From<TypeMetadata> for Type {
     fn from(value: TypeMetadata) -> Self {
         match value {
+            TypeMetadata::u8 => Type::u8(0),
             TypeMetadata::u16 => Type::u16(0),
             TypeMetadata::u32 => Type::u32(0),
             TypeMetadata::u64 => Type::u64(0),
+
+            TypeMetadata::i8 => Type::i8(0),
             TypeMetadata::i16 => Type::i16(0),
             TypeMetadata::i32 => Type::i32(0),
             TypeMetadata::i64 => Type::i64(0),
+
             TypeMetadata::ptr => Type::ptr(0),
             TypeMetadata::Void => Type::Void,
         }
